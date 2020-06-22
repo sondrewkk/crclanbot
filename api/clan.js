3f11021cdfaee5a965d25b04aafe5a919909f128
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2020-06-22 21:20:10 +0200

    Added warlog endpoint to clan api

--- api/clan.js
+++ api/clan.js
@@ -1,14 +1,13 @@
 const axios = require('axios');
 
+const options = {
+  headers: {
+    'Authorization': `Bearer ${process.env.CR_API_TOKEN}`
+  }
+};
+
 module.exports = {
   async members(tag) {
-    
-    const options = {
-      headers: {
-        'Authorization': `Bearer ${process.env.CR_API_TOKEN}`
-      }
-    };
-
     try {
       const res = await axios.get(`https://${process.env.CR_API_URL}/clans/%23${tag}/members`, options);
       const members = res.data.items;
@@ -16,5 +15,13 @@ module.exports = {
       return members;
     } catch (err) { console.log(`Error: ${err}`) }
     
+  },
+  async warlog(tag) {
+    try {
+      const res = await axios.get(`https://${process.env.CR_API_URL}/clans/%23${tag}/warlog`, options);
+      const warlog = res.data.items;
+
+      return warlog;
+    } catch (err) { console.log(`Error: ${err}`) }
   }
 }
