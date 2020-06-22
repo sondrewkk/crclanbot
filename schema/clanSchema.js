2972e8365700597235da46a4253d980ddf501da3
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2020-06-22 21:20:57 +0200

    Changed previousRun to number to store value of a moment

--- schema/clanSchema.js
+++ schema/clanSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ClanSchema = new mongoose.Schema({
   tag: String,
   interval: Number,
-  previousRun: { type: Date, default: 0},
+  previousRun: { type: Number, default: 0},
   channelId: String
 });
 
